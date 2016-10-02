9b451f19fab98930c7c9c9faa599703f6b527d79
valentinp72 <valentinpelloin@gmail.com>
2016-10-02 16:00:18 +0200

    Bug de la réouverture automatique du menu si clic dessus corrigé
    Ajout d’une condition qui vérifie que le menu est fermé pour l’ouvrir.

--- menu.js
+++ menu.js
@@ -1,28 +1,35 @@
 jQuery(document).ready(function () {
 
+    var menuOuvert = false;
+
     //Ouverture du menu
     jQuery("#hamburger").click(function () {
+        
+        if(menuOuvert == false) { //Permet d'empecher la ré-ouverture du menu si il est déjà ouvert
+            //On affiche le menu
+            jQuery('#menu-hamburger').css('opacity', 1);
+            jQuery('#menu-hamburger').css('display', 'block');
 
-        //On affiche le menu
-        jQuery('#menu-hamburger').css('opacity', 1);
-        jQuery('#menu-hamburger').css('display', 'block');
+            //On fixe le contenu de la page, pour empecher le scroll
+            jQuery('#pageContent').css('position', 'fixed');
+            jQuery('#contentLayer').css('display', 'block');
 
-        //On fixe le contenu de la page, pour empecher le scroll
-        jQuery('#pageContent').css('position', 'fixed');
-        jQuery('#contentLayer').css('display', 'block');
+            //Ouverture de l'anti-scroll : décalage vers la gauche, permet de suivre le reste du contenu pour pouvroir cliquer dessus
+            jQuery('#contentLayer').addClass('open');
+            //Ouverture du contenu de la page : décalage vers la gauche
+            jQuery('#pageContent').addClass('open');
+           
 
-        //Ouverture de l'anti-scroll : décalage vers la gauche, permet de suivre le reste du contenu pour pouvroir cliquer dessus
-        jQuery('#contentLayer').addClass('open');
-        //Ouverture du contenu de la page : décalage vers la gauche
-        jQuery('#pageContent').addClass('open');
-       
+            
+            //Désactivation du scroll sur les mobiles
+            jQuery('#pageContent').bind('touchmove', function (e) {
+                e.preventDefault()
+            });
+            menuOuvert = true;
+        }
+        
 
         
-        //Désactivation du scroll sur les mobiles
-        jQuery('#pageContent').bind('touchmove', function (e) {
-            e.preventDefault()
-        });
-
 
     });
 
@@ -30,6 +37,7 @@ jQuery(document).ready(function () {
     jQuery(document).on('click touchstart', function(event){
 
         if(jQuery(event.target).closest('#contentLayer').length && !$(event.target).closest("#hamburger").length) {
+
 
             jQuery('#pageContent').addClass('close');
             jQuery('#pageContent').removeClass('open');
@@ -40,6 +48,7 @@ jQuery(document).ready(function () {
                 jQuery('#menu-hamburger').css('opacity', 0);
                 jQuery('#pageContent').removeClass('close');
                 jQuery('#menu-hamburger').css('display', 'none');
+                menuOuvert = false;
             }, 700);
 
             //On ré-autorise le scroll de la page
