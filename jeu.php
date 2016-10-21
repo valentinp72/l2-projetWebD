302685a2ab390562936050724b466b898bc56a83
TitouanT <titouan.teyssier@gmail.com>
2016-10-21 09:54:57 +0200

    update jeu.php

--- jeu.php
+++ jeu.php
@@ -1,49 +1,46 @@
 <?php
-	$title = "Jeu";
+	$titrePage = "Jeu";
 	include('header.php');
 	include('hierarchie.php')
 ?>
 
 
 
-		        <div id="titre">Monopoly</div>
+<div id="titre">Monopoly</div>
 
-				<div id="imageDeJeu">
-						<img src="media/images_catalogue/monopoly.jpg" alt="illustration monopoly"/>
-				</div>
+<div id="imageDeJeu">
+	<img src="media/images_catalogue/monopoly.jpg" alt="illustration monopoly"/>
+</div>
 
-				<div id="content">
+<div id="content">
 
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
-					</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus commodo ullamcorper. Pellentesque placerat, nunc eget rutrum finibus, eros leo elementum risus, vel eleifend ante eros id lorem. Ut quis rhoncus elit. Cras a dolor eget odio luctus consequat sit amet eu eros. Donec interdum orci nunc, quis viverra libero varius in. Quisque sollicitudin quis sapien ac ornare. Morbi efficitur urna at sem aliquet, at malesuada diam ultrices.
+	</p>
+</div>
 
-
-
-				</div>
-
-	<?php include('footer.php'); ?>
+<?php include('footer.php'); ?>
