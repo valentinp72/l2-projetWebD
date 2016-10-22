730bb9fb6eaaf489bee1ed70b27e15b5a1c462d7
Valentin Pelloin <valentinpelloin@gmail.com>
2016-10-22 16:04:25 +0200

    Ajout du champ sujet aux mails

--- nous_contacter.php
+++ nous_contacter.php
@@ -9,7 +9,7 @@ include('hierarchie.php')
 	<h1>Nous contacter</h1>
 
 	<?php
-	if(!isset($_POST["submit"]) or empty($_POST["mail"]) or empty($_POST["message"]) or !filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL) ){
+	if(!isset($_POST["submit"]) or empty($_POST["mail"]) or empty($_POST["message"]) or empty($_POST["sujet"]) or !filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL) ){
 
 		if(!empty($_POST['message']) and !filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL) ){
 			echo "Votre adresse mail est invalide.";
@@ -25,8 +25,12 @@ include('hierarchie.php')
 				<input type="email" id="mail" name="mail" placeholder="Ex : bibi@exemple.com" value="<?php echo $_POST['mail']; ?>" required>
 			</p>
 			<p>
+				<label for="sujet">Sujet du mail : </label>
+				<input id="sujet" name="sujet" placeholder="Ex : Problème de réservation" value="<?php echo $_POST['sujet']; ?>" required>
+			</p>
+			<p>
 				<label for="message">Votre message : </label>
-				<textarea name="message" id="message" placeholder="Ex: Bonjour, blablabla." rows="10" cols="50" required><?php echo $_POST['message']; ?></textarea>
+				<textarea name="message" id="message" placeholder="Ex: Bonjour, j'ai un problème pour réserver un jeu..." rows="10" cols="50" required><?php echo $_POST['message']; ?></textarea>
 			</p>
 			<p>
 				<input type="submit" name="submit" id="submit" value="Envoyer mon message">
@@ -38,10 +42,10 @@ include('hierarchie.php')
 		else{
 
 			$destinataire = "valentinpelloin@gmail.com";
-			$sujet = "Nouveau mail sur ludoTEK !";
+			$message = $_POST["message"] + "\r\n\r\n------\r\nCe mail a été envoyé depuis le site http://ludotheque.vlntn.pw/";
 			$headers = "From: " . $_POST["mail"] . "\r\n";
 
-			$retour = mail($destinataire,$sujet,$_POST["message"],$headers);
+			$retour = mail($destinataire,$_POST["sujet"],$message,$headers);
 
 			if($retour){
 				echo "Votre mail a bien été envoyé aux administrateurs !";
