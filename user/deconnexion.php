28be151aab894e51ae87d6af7eba21d324e9a7e1
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-08 22:14:27 +0100

    deconnexion save panier test 2

--- user/deconnexion.php
+++ user/deconnexion.php
@@ -4,7 +4,7 @@ require '_connexionBDD.php';
 
 $n = count($_SESSION['panier']['produit']);
 if ($n > 0) {
-	for ($i = 0; i < $n; $i++) {
+	for ($i = 0; $i < $n; $i++) {
 		$requete = mysql_query("INSERT INTO VR_grp14_Panier (ID_Jeu, ID_Client) VALUES ('".$_SESSION['panier']['id'][$i]."', '".$_SESSION['UserID']."')");
 
 		if(!$requete) {
