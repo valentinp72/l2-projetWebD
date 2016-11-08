6e90954e0fcef8bd5ed216eb673164004bfa4f5c
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-08 21:08:19 +0100

    Update _connexionAdminRequise.php

--- admin/_connexionAdminRequise.php
+++ admin/_connexionAdminRequise.php
@@ -4,7 +4,7 @@ session_start();
 if(!isset($_SESSION['userID'])){
   header('location: ../user/connexion.php'); //Si l'utilisateur n'est pas connecté, on le redirige vers la page de connexion
 }
-if($_SESSION['admin'] != 1){
+else if($_SESSION['admin'] != 1){
   header('location: ../index.php');  //L'utilisateur doit être admin
 }
 ?>
