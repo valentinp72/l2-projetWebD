90c203113af0bc57b3cdfdea46c7107237dc67b6
Titouan Teyssier <titouan.teyssier@gmail.com>
2016-11-08 22:21:38 +0100

    deconnexion save panier test 5

--- user/deconnexion.php
+++ user/deconnexion.php
@@ -6,7 +6,7 @@ require $rootURL . '_connexionBDD.php';
 $n = count($_SESSION['panier']['produit']);
 if ($n > 0) {
 	for ($i = 0; $i < $n; $i++) {
-		$requete = mysql_query("INSERT INTO VR_grp14_Panier (ID_Jeu, ID_Client) VALUES ('".$_SESSION['panier']['id'][$i]."', '".$_SESSION['UserID']."')");
+		$requete = mysql_query("INSERT INTO VR_grp14_Paniers (ID_Jeu, ID_Client) VALUES ('".$_SESSION['panier']['id'][$i]."', '".$_SESSION['UserID']."')");
 
 		if(!$requete) {
 			die('Erreur dans la requête : ' . mysql_error());
@@ -17,7 +17,6 @@ $_SESSION = array();
 session_destroy();
 
 $titrePage = "Déconnexion";
-$rootURL = "../";
 include($rootURL . '_header.php');
 include($rootURL . '_hierarchie.php')
 ?>
